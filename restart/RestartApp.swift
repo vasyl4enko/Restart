6b76af0ca59571b56aef4bc11b04eb78f69c8710
Oleksandr <oleksandr@MacBook-Pro-Oleksandr.local>
2023-06-28 16:10:13 +0300

    init

---
+++ restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  restartApp.swift
+//  restart
+//
+//  Created by Oleksandr on 28.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
